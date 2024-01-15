2b3cd2744973c7c7fe2c62a60512eac19a0773ec
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2024-01-15 18:25:20 +0200

    added basics files

---
+++ src/main/java/project/mybookingapp/MyBookingAppApplication.java
@@ -0,0 +1,13 @@
+package project.mybookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/mybookingapp/MyBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package project.mybookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
