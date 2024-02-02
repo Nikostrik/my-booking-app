66528c0e166201fe24c304b2550cf56f8d1fb06c
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2024-02-02 11:57:36 +0200

    added maven checkstyle plugin and configured Cl process

--- src/main/java/project/mybookingapp/MyBookingAppApplication.java
+++ src/main/java/project/mybookingapp/MyBookingAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyBookingAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MyBookingAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MyBookingAppApplication.class, args);
+    }
 }
